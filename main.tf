9e047ba2fb26651c794b595c7f95fae889ba6c97
revatgit <103292267+revatgit@users.noreply.github.com>
2025-10-22 11:46:31 -0400

    use module for security groups

--- main.tf
+++ main.tf
@@ -22,12 +22,28 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.web.id]
+  vpc_security_group_ids = [module.web_sg.security_group_id ]
 
   tags = {
     Name = "HelloWorld"
   }
 }
+
+
+
+module "web_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.1"
+  name = web-new
+  vpc_id  = data.aws_vpc.default.id
+
+  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks  =   ["0.0.0.0/0"]
+
+  egress_rules = ["all-all"]
+  egress_cidr_blocks  =   ["0.0.0.0/0"]
+}
+
 
 resource "aws_security_group" "web" {
   name = "web"
