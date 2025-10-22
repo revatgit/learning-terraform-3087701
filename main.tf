2af41875045db33fa05eee8cad3a9960d15acdc3
revatgit <103292267+revatgit@users.noreply.github.com>
2025-10-22 11:51:05 -0400

    use module for security group-correct spelling

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "web" {
 module "web_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.1"
-  name = web-new
+  name = "web-new"
   vpc_id  = data.aws_vpc.default.id
 
   ingress_rules = ["http-80-tcp","https-443-tcp"]
