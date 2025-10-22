ed373cfed3fa1c87841922fbda240ea44ee80b43
revatgit <103292267+revatgit@users.noreply.github.com>
2025-10-22 12:38:12 -0400

    load balancer

--- main.tf
+++ main.tf
@@ -49,6 +49,54 @@ resource "aws_instance" "web" {
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "mweb-alb"
+  vpc_id  = module.web_vpc.vpc_id
+
+  subnets =module.web_vpc.public_subnets
+  security_groups = module.web_sg.security_group_id 
+
+ 
+  
+
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+    ex-https = {
+      port            = 443
+      protocol        = "HTTPS"
+      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+
+      forward = {
+        target_group_key = "ex-instance"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "web-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.web.id
+    }
+  }
+
+  tags = {
+    Environment = "Development"
+    Project     = "Example"
+  }
+}
 
 
 module "web_sg" {
